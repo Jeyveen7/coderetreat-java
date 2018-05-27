f77fc56441cf5f9567d37d4f9e0e5791738b12d5
Justin Wyer <justin@lifeasageek.com>
2018-05-27 16:17:20 +0200

    update to junit 5 / java 10

--- src/test/java/org/coderetreat/AppTest.java
+++ src/test/java/org/coderetreat/AppTest.java
@@ -1,6 +1,7 @@
 package org.coderetreat;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
